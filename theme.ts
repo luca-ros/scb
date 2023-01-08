a3f96ea3a8eee79e9e5956ec56e2410782c64f19
luca-ros <lucattparte@gmail.com>
2023-01-08 18:25:56 +0100

    renamed brand

--- theme.ts
+++ theme.ts
@@ -3,7 +3,7 @@ import { buildLegacyTheme, defineConfig } from "sanity";
 const props = {
   "--my-white": "#F5F5F5",
   "--my-black": "#262626",
-  "--papafam-brand": "#F7AB0A",
+  "--kalu-brand": "#F7AB0A",
   "--my-red": "#db4437",
   "--my-yellow": "#f4b400",
   "--my-green": "#0f9d58",
@@ -21,17 +21,17 @@ export const myTheme = buildLegacyTheme({
   "--component-text-color": props["--my-white"],
 
   /* Brand */
-  "--brand-primary": props["--papafam-brand"],
+  "--brand-primary": props["--kalu-brand"],
 
   // Default button
   "--default-button-color": "#666",
-  "--default-button-primary-color": props["--papafam-brand"],
+  "--default-button-primary-color": props["--kalu-brand"],
   "--default-button-success-color": props["--my-green"],
   "--default-button-warning-color": props["--my-yellow"],
   "--default-button-danger-color": props["--my-red"],
 
   /* State */
-  "--state-info-color": props["--papafam-brand"],
+  "--state-info-color": props["--kalu-brand"],
   "--state-success-color": props["--my-green"],
   "--state-warning-color": props["--my-yellow"],
   "--state-danger-color": props["--my-red"],
@@ -40,5 +40,5 @@ export const myTheme = buildLegacyTheme({
   "--main-navigation-color": props["--my-black"],
   "--main-navigation-color--inverted": props["--my-white"],
 
-  "--focus-color": props["--papafam-brand"],
+  "--focus-color": props["--kalu-brand"],
 });

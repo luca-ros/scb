449dd0ca1be9ce3ebd4c28ad42fdc5e404f08547
Luca Rosano <92313604+luca-ros@users.noreply.github.com>
2024-02-05 09:22:06 +0100

    Update layout.tsx

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -2,6 +2,7 @@ import Banner from "../../components/Banner";
 import Header from "../../components/Header";
 import "../../styles/globals.css";
 
+
 export default function RootLayout({
   children,
 }: {
